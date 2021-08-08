75d85dcf3fe721327b26affaf23353e54375adf0
pavanyalala <pavanyalala4508@gmail.com>
2021-08-08 13:14:56 +0530

    [pavan]:Stack creation is added

--- StackQueueProgram/Program.cs
+++ StackQueueProgram/Program.cs
@@ -8,6 +8,11 @@ namespace StackQueueProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack Queue Program");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
 }
---
+++ StackQueueProgram/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueueProgram
+{
+    class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        /// <summary>
+        /// add value to stack
+        /// </summary>
+        /// <param name="value"></param>
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        /// <summary>
+        /// display stack
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
